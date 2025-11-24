631425d36bc66e79316e8dd2d98ff3f6fa918fe1
slafontd <slafontd@eafit.edu.co>
2025-11-24 11:56:38 -0500

    Opción de pasar productos por teclado

--- ModuloWeb.BROKER/obj/Debug/net8.0/ModuloWeb.BROKER.AssemblyInfo.cs
+++ ModuloWeb.BROKER/obj/Debug/net8.0/ModuloWeb.BROKER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloWeb.BROKER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a1ad6a513b415662068c70ed88818844867f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788b8ccfb16232d51db77beb6e5bca7ed3dd736a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloWeb.BROKER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloWeb.BROKER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
