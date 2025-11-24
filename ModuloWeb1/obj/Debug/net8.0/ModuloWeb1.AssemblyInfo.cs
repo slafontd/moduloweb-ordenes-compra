184b3a18dee4a06383761660fd44a9022ef952d1
slafontd <slafontd@eafit.edu.co>
2025-11-24 01:30:30 -0500

    Manejo de plantilla de Excel

--- ModuloWeb1/obj/Debug/net8.0/ModuloWeb1.AssemblyInfo.cs
+++ ModuloWeb1/obj/Debug/net8.0/ModuloWeb1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloWeb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a1ad6a513b415662068c70ed88818844867f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b493a0b6148ad41ccacb36ae9d1cf93053e98f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloWeb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloWeb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
